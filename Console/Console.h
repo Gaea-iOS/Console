10b2ea9e437083ebd05ffc18b22387504adb054a
jerry <445242970@qq.com>
2017-06-06 15:51:40 +0800

    first commit

---
+++ Console/Console.h
@@ -0,0 +1,19 @@
+//
+//  Console.h
+//  Console
+//
+//  Created by 王小涛 on 2017/6/6.
+//  Copyright © 2017年 王小涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Console.
+FOUNDATION_EXPORT double ConsoleVersionNumber;
+
+//! Project version string for Console.
+FOUNDATION_EXPORT const unsigned char ConsoleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Console/PublicHeader.h>
+
+
